d247bd7501625ff9fd049451db93fc07f62b613e
uma13 <umaveerapaneni@gmail.com>
2024-05-08 11:47:42 +0530

    commit

---
+++ EventModule-1/src/main/java/com/uma/linkdlen/EventModule1Application.java
@@ -0,0 +1,13 @@
+package com.uma.linkdlen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventModule1Application.class, args);
+	}
+
+}
